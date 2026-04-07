805a5641c064dc0329f8e39cfb3c82108fd7249b
callme.waffle <nate.crema@gmail.com>
2026-04-07 20:58:00 +0900

    feat: add team Notion fetchers for crew, activity and project

---
+++ src/modules/team/notion/fetchers/activity.fetcher.ts
@@ -0,0 +1,19 @@
+import { Client } from '@notionhq/client';
+import type { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
+
+export class ActivityFetcher {
+  constructor(
+    private readonly notionClient: Client,
+    private readonly databaseId: string,
+  ) {}
+
+  async fetchAll(): Promise<PageObjectResponse[]> {
+    // TODO: 100건 초과 시 start_cursor를 이용한 페이지네이션 처리 필요
+    const response = await this.notionClient.databases.query({
+      database_id: this.databaseId,
+    });
+    return response.results.filter(
+      (page): page is PageObjectResponse => page.object === 'page' && 'properties' in page,
+    );
+  }
+}
